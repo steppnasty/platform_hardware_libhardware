617888e69c27dabaa92af15543c9a028388a0059
steppnasty <steppnasty@gmail.com>
2014-07-28 13:53:41 -0400

    gralloc: Merged updates from jb-mr1.1-release branch

--- include/hardware/gralloc.h
+++ include/hardware/gralloc.h
@@ -80,8 +80,12 @@ enum {
     GRALLOC_USAGE_HW_CAMERA_WRITE       = 0x00020000,
     /* buffer will be read by the HW camera pipeline */
     GRALLOC_USAGE_HW_CAMERA_READ        = 0x00040000,
+    /* buffer will be used as part of zero-shutter-lag queue */
+    GRALLOC_USAGE_HW_CAMERA_ZSL         = 0x00060000,
+    /* mask for the camera access values */
+    GRALLOC_USAGE_HW_CAMERA_MASK        = 0x00060000,
     /* mask for the software usage bit-mask */
-    GRALLOC_USAGE_HW_MASK               = 0x00011F00,
+    GRALLOC_USAGE_HW_MASK               = 0x00071F00,
 
     /* buffer should be displayed full-screen on an external display when
      * possible
@@ -262,4 +266,4 @@ static inline int gralloc_close(struct alloc_device_t* device) {
 
 __END_DECLS
 
-#endif  // ANDROID_ALLOC_INTERFACE_H
+#endif  // ANDROID_GRALLOC_INTERFACE_H
